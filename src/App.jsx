24197a27cc5d367fbba6e2b82968dd3079f5b8e2
K Briaan <kbriaan1@gmail.com>
2024-07-19 13:48:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from './components/Hero/Hero'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div>
+ <Hero/>
+ <Navbar/>
+    </div>
+   
+  )
+}
+
+export default App
